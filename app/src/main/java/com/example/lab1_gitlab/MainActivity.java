291837ad8a6bfa9787fd14e360015c953f5e39ed
Albina Sorokina <albinawolf@gmail.ru>
2020-09-26 20:07:32 +0700

    Init

---
+++ app/src/main/java/com/example/lab1_gitlab/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.lab1_gitlab;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+
+
+
+
+/*class MinMaxExample1 {
+
+
+
+    public static void main(String args[]){
+
+        int array[] = new int[]{10, 11, 88, 2, 12, 120};
+
+        // Вызов метода getMax () для получения максимального значения
+        int max = getMax(array);
+        //System.out.println("Maximum Value is: "+max);
+
+        // Вызов метода getMin () для получения минимального значения
+        int min = getMin(array);
+        //System.out.println("Minimum Value is: "+min);
+
+
+    }
+    //здесь находим максимум
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){ if(inputArray[i] > maxValue){
+            maxValue = inputArray[i];
+        }
+        }
+        return maxValue;
+    }
+
+    // здесь находим минимум
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
+}
+
+*/
